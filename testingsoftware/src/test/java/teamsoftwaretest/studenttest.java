4787eb8f745749bda5ca81a6bd7778f153f56d5d
mommy742 <grant.walker581@gmail.com>
2020-04-03 14:29:19 -0400

    java test cases run by tasks in gradle can talk to the h2 server with sql statements and return answers. i am a god!!

--- testingsoftware/src/test/java/teamsoftwaretest/studenttest.java
+++ testingsoftware/src/test/java/teamsoftwaretest/studenttest.java
@@ -5,17 +5,19 @@ import java.sql.*;
 
 
 public class studenttest {
-//    @Test
-//    public void testing1(){
-//        Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa","");
-//        PreparedStatement stmt = null;
-//        ResultSet rs = null;
-//        String p =" select * "
-//                + " from STUDENT "
-//                + " where ID = ?";
-//        stmt = conn.prepareStatement(p);
-//        stmt.setInt(1, studentID);
-//        rs = stmt.executeQuery();
-//        System.out.println(rs.getInt());
-//    }
+    @Test
+    public void testing1() throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa","");
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String p =" select * "
+                + " from STUDENT "
+                + " where ID = ?";
+        stmt = conn.prepareStatement(p);
+        stmt.setInt(1, 123);
+        rs = stmt.executeQuery();
+        rs.next();
+        System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " +  rs.getString(3));
+    }
 }
